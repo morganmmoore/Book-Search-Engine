2d9d41a4b48afad51b19892c0bf6782c16bdabdd
morganmmoore <morganmmoore0@gmail.com>
2022-01-11 20:30:03 -0500

    Start of resolvers.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,36 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        email: String
+        username: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book {
+        _id: ID
+        authors: [String]
+        bookId: String
+        description: String
+        image: String
+        title: String
+        link: String
+        forSale: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        me: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(authors: [String], description: String, title: String, bookId: String, image: String, link: String): User
+        removeBook(bookId: String): User
+    }
+`;
+
+module.exports = typeDefs;
